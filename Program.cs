691450440c1b6ca2c94db85616447dfae9b89d2f
Carter Croft <80287993+cartercroft@users.noreply.github.com>
2023-05-11 01:03:46 -0500

    Ability to Generate Scale for Base Frequency
    Added GetMajorScaleFrequenceies(float) which returns an array of 8 floats being the frequencies of the 7 notes in the major scale plus the root note one octave higher.

--- Program.cs
+++ Program.cs
@@ -40,51 +40,21 @@ using(FileStream fs = new FileStream(path, FileMode.Create))
         //Done writing file header
 
         double multiplier = 2.0 * Math.PI / SAMPLERATE;
-        int volume = 10;
+        int volume = 2;
         for(int i = 0; i < data.Length; i++)
         {
             data[i] = 128;
         }
         int oneEighth = data.Length / 8, count = 0;
-        while(count < oneEighth)
+        float baseFreq = 532.28f;
+
+        float[] scaleFrequencies = GetMajorScaleFrequencies(baseFreq);
+        int scaleFrequencyReadIndex = 0;
+        for(int i = 0; i < data.Length; i++)
         {
-            data[count] = (byte)(data[count] + volume * Math.Sin(count * multiplier * 523.28));
-            count++;
-        }
-        while (count < oneEighth * 2)
-        {
-            data[count] = (byte)(data[count] + volume * Math.Sin(count * multiplier * 587.36));
-            count++;
-        }
-        while (count < oneEighth * 3)
-        {
-            data[count] = (byte)(data[count] + volume * Math.Sin(count * multiplier * 659.28));
-            count++;
-        }
-        while (count < oneEighth * 4)
-        {
-            data[count] = (byte)(data[count] + volume * Math.Sin(count * multiplier * 698.48));
-            count++;
-        }
-        while (count < oneEighth * 5)
-        {
-            data[count] = (byte)(data[count] + volume * Math.Sin(count * multiplier * 784.0));
-            count++;
-        }
-        while (count < oneEighth * 6)
-        {
-            data[count] = (byte)(data[count] + volume * Math.Sin(count * multiplier * 880.0));
-            count++;
-        }
-        while (count < oneEighth * 7)
-        {
-            data[count] = (byte)(data[count] + volume * Math.Sin(count * multiplier * 987.84));
-            count++;
-        }
-        while (count < data.Length)
-        {
-            data[count] = (byte)(data[count] + volume * Math.Sin(count * multiplier * 1046.56));
-            count++;
+            if(i != 0 && i % oneEighth == 0)
+                scaleFrequencyReadIndex++;
+            data[i] = (byte)(data[count] + volume * Math.Sin(i * multiplier * scaleFrequencies[scaleFrequencyReadIndex]));
         }
         bw.Write(data);
     }
@@ -93,4 +63,25 @@ using(FileStream fs = new FileStream(path, FileMode.Create))
 static byte[] GetBytes(string str)
 {
     return Encoding.ASCII.GetBytes(str);
+}
+
+static float[] GetMajorScaleFrequencies(float baseFreq) 
+{
+    string intervals = "WWHWWWH";
+    float[] freqs = new float[8];
+    freqs[0] = baseFreq;
+    for (int i = 1; i < freqs.Length; i++)
+    {
+        float factor;
+        
+        if (intervals[0] == 'W')
+            factor = (float)Math.Pow(2.0, 2/12.0f);
+        else
+            factor = (float)Math.Pow(2.0, 1 /12.0f);
+        
+        freqs[i] = freqs[i - 1] * factor;
+        intervals = intervals.Substring(1);
+    }
+
+    return freqs;
 }
